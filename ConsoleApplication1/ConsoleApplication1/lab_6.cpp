f232bd3d9575e1da578b0443b46ac4b61eebd57b
amasterous <pomnitnado@yandex.ru>
2018-10-19 19:50:53 +0300

    qwewqeqw

--- ConsoleApplication1/ConsoleApplication1/lab_6.cpp
+++ ConsoleApplication1/ConsoleApplication1/lab_6.cpp
@@ -26,7 +26,24 @@ void output_array(float arr[10][10], int kolvo_strok, int kolvo_stolbcov) {
 	}
 }
 
-float max_elem
+float max_elem(float arr[10][10], int kolvo_strok, int kolvo_stolbcov) {
+	int max = arr[0][0];
+	for (int i = 0; i < kolvo_strok; i++) {
+		for (int j = 0; j < kolvo_stolbcov; j++) {
+			if (arr[i][j] > max) {
+				max = arr[i][j];
+			}
+		}
+	}
+	return max;
+}
+
+void change_elems(float arr[10][10], int i, int j, int i2, int j2) {
+	float varr;
+	varr = arr[i][j];
+	arr[i][j] = arr[i2][j2];
+	arr[i2][j2] = varr;
+}
 
 int main() {
 	setlocale(LC_CTYPE, "");
@@ -44,5 +61,16 @@ int main() {
 
 	output_array(arr, kolvo_strok, kolvo_stolbcov);
 
+	printf("max = %f", max_elem(arr, kolvo_strok, kolvo_stolbcov));
+	int x, xx, y, yy;
+	printf("введите номер строки: ");
+	scanf_s("%f", &x);
+	printf("введите номер столбец: ");
+	scanf_s("%f", &xx);
+	printf("введите номер строки(2): ");
+	scanf_s("%f", &x);
+	printf("введите номер столбец(2): ");
+	scanf_s("%f", &xx);
+	printf("%f");
 
 }

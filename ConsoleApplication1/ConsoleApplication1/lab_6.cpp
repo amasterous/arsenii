d53336aedea0a683efe2ade05112621da924a354
amasterous <pomnitnado@yandex.ru>
2018-10-19 18:11:36 +0300

    qweqwee

--- ConsoleApplication1/ConsoleApplication1/lab_6.cpp
+++ ConsoleApplication1/ConsoleApplication1/lab_6.cpp
@@ -8,13 +8,41 @@
 #include <iostream> 
 
 
-/*
-Дан массив 10 целые числа онли
-1) удалить из массива (а) первые 5 чисел , массив сдвигается
-2)массив (б) 10 элементов . Ввести с клавы массив (б). Элемент массива b(h)[i]=a[i]*b[i]
-3)Сортировка нового а[ ] по возрастанию
-*/
+float input_array(float arr[10][10],int kolvo_strok,int kolvo_stolbcov) {
+
+	for (int i = 0; i < kolvo_strok; i++) {
+		for (int j = 0; j < kolvo_stolbcov; j++) {
+			arr[i][j] = i * j;
+		}
+	}
+	return arr[kolvo_strok][kolvo_stolbcov];
+}
+
+void output_array(float arr[10][10], int kolvo_strok, int kolvo_stolbcov) {
+	for (int i = 0; i < kolvo_strok; i++) {
+		for (int j = 0; j < kolvo_stolbcov; j++) {
+			printf("[%d][%d] = %f\n", i, j, arr[i][j]);
+		}
+	}
+}
+
+float max_elem
 
 int main() {
-	
+	setlocale(LC_CTYPE, "");
+
+	float arr[10][10];
+
+	int kolvo_strok;
+	int kolvo_stolbcov;
+	printf("введите кол-во строк (<10)");
+	scanf_s("%d", &kolvo_strok);
+	printf("введите кол-во столбцов (<10)");
+	scanf_s("%d", &kolvo_stolbcov);
+
+	input_array(arr,kolvo_strok, kolvo_stolbcov);
+
+	output_array(arr, kolvo_strok, kolvo_stolbcov);
+
+
 }
